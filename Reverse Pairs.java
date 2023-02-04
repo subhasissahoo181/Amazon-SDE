2d702a96ea7e4fc877079c781dd741e5d083adb5
Subhasis sahoo <76907034+subhasissahoo181@users.noreply.github.com>
2023-02-04 04:08:35 -0800

    Add files via upload

--- Reverse Pairs.java
+++ Reverse Pairs.java
@@ -11,3 +11,52 @@ class Solution {
         return Pairs;
     }
 }
+//-------------------------------------------------------------------------
+class Solution {
+    public int reversePairs(int[] nums) {
+        return mergSort(nums, 0, nums.length-1);
+        
+    }
+    static int merg(int[] nums, int low, int mid, int high){
+        int count =0;
+        int j=mid +1;
+        for(int i=low; i<=mid;i++){
+            while(j<=high && nums[i]> (2*(long)nums[j])){
+                j++;
+            }
+            count = count+(j-(mid+1));
+        }
+        ArrayList<Integer> temp = new ArrayList<Integer>();
+        int left =low, right = mid+1;
+        while(left<= mid && right <= high){
+            if(nums[left] <= nums[right]){
+                temp.add(nums[left++]);
+            }
+            else{
+                temp.add(nums[right++]);
+            }
+        }
+        while(left<=mid){
+            temp.add(nums[left++]);
+        }
+        while(right<=high){
+            temp.add(nums[right++]);
+        }
+        for(int i=low; i<=high; i++){
+            nums[i] = temp.get(i-low);
+        }
+        return count;
+    }
+    static int mergSort(int[] nums, int low, int high){
+        if(low >= high){
+            return 0;
+        }
+        int mid = (low +high)/2;
+        int inverse = mergSort(nums,low,mid);
+        inverse = inverse + mergSort(nums,mid+1,high);
+        inverse = inverse + merg(nums,low,mid,high);
+        return inverse;
+    }
+}
+
+
